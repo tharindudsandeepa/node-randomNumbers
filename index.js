6c2b9d71897693676e71a364de22c2a46b820f51
Tharindu Sandeepa <73959253+tharindudsandeepa@users.noreply.github.com>
2023-05-24 16:55:43 +0530

    generate unique charactors by using uuid

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const {v4:uuidv4}=require('uuid');
+
+function generateNumbers(count){
+    const numbers=new Set();
+
+    while(numbers.size<count){
+      const randomNumber=uuidv4();
+      numbers.add(randomNumber);  
+    }
+
+    return Array.from(numbers);
+}
+
+const count=100;
+const uniqueNumbers=generateNumbers(count);
+console.log(uniqueNumbers);
