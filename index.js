c708852a24aea271d5b429c43313ee5d5e75727e
Tharindu Sandeepa <73959253+tharindudsandeepa@users.noreply.github.com>
2023-05-24 17:36:31 +0530

    generate unique numbers and formatted it.

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const {v4:uuidv4}=require('uuid');
 
 function generateNumbers(count){
     const numbers=new Set();
+    const formatted=new Set();
 
     while(numbers.size<count){
-      const randomNumber=uuidv4();
-      numbers.add(randomNumber);  
+      const randomNumber=uuidv4().replace(/-/g,'').replace(/\D/g,'').substring(0,8);
+      const formattedNumber = `${randomNumber.substring(0, 2)}-${randomNumber.substring(2, 5)}-${randomNumber.substring(5)}`;
+      numbers.add(formattedNumber);  
     }
 
     return Array.from(numbers);
 }
+// const random ='123456789123'
+// console.log(`${random.substring(0,2)}-${random.substring(2,5)}-${random.substring(5,8)}`);
 
-const count=10;
+const count=50;
 const uniqueNumbers=generateNumbers(count);
 console.log(uniqueNumbers);
